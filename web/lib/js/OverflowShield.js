012b4548ea56862efe5390fe117e69152d1f98c9
Giulio Bosco <giuliobva@gmail.com>
2018-10-19 13:27:10 +0200

    add front-end libraries
    angularjs, angularjs-material, bootstrap, drp, help, overflowshield, prettyphoto, font-awesom, jquery (minm parallax, prettyphoto, scrollify)

---
+++ web/lib/js/OverflowShield.js
@@ -0,0 +1,16 @@
+/**
+ * @author Giulio Bosco (giuliobva@gmail.com)
+ * @version 1.0 (27.02.2018)
+ * @requires jQuery 1.11.1 (jquery1.11.1.js)
+ * @requires OverflowShield CSS (OverflowShield.css)
+ * @requires Bootstrap v3.3.2
+ */
+
+function showShield(id) {
+	$('#' + id).show();
+	$('body').css('overflow', 'none');
+}
+
+function hideShield(id) {
+	$('#' + id).hide();
+}
