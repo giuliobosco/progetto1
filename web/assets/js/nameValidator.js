4024fb9061de5dba9610d1e9bdd38216a83c6eab
Giulio Bosco <giuliobva@gmail.com>
2018-10-24 13:54:34 +0200

    add js nameValidation

---
+++ web/assets/js/nameValidator.js
@@ -0,0 +1,27 @@
+/**
+ * Name Validator.
+ *
+ * @author giuliobosco
+ * @verision 1.0
+ */
+
+/**
+ * Name Validator.
+ * @type {NameValidator}
+ */
+let nameValidator = class NameValidator extends Validator {
+	/**
+	 * Validate email.
+	 *
+	 * @param name {string} Name to validate.
+	 * @returns {boolean} Result of the validation.
+	 */
+	isValid(name) {
+		if (!(name.trim().length > this.min && name.trim().length < this.max)) {
+			return false;
+		}
+
+		let regex = /^[a-zA-Z\-_ ’'‘ÆÐƎƏƐƔĲŊŒẞÞǷȜæðǝəɛɣĳŋœĸſßþƿȝĄƁÇĐƊĘĦĮƘŁØƠŞȘŢȚŦŲƯY̨Ƴąɓçđɗęħįƙłøơşșţțŧųưy̨ƴÁÀÂÄǍĂĀÃÅǺĄÆǼǢƁĆĊĈČÇĎḌĐƊÐÉÈĖÊËĚĔĒĘẸƎƏƐĠĜǦĞĢƔáàâäǎăāãåǻąæǽǣɓćċĉčçďḍđɗðéèėêëěĕēęẹǝəɛġĝǧğģɣĤḤĦIÍÌİÎÏǏĬĪĨĮỊĲĴĶƘĹĻŁĽĿʼNŃN̈ŇÑŅŊÓÒÔÖǑŎŌÕŐỌØǾƠŒĥḥħıíìiîïǐĭīĩįịĳĵķƙĸĺļłľŀŉńn̈ňñņŋóòôöǒŏōõőọøǿơœŔŘŖŚŜŠŞȘṢẞŤŢṬŦÞÚÙÛÜǓŬŪŨŰŮŲỤƯẂẀŴẄǷÝỲŶŸȲỸƳŹŻŽẒŕřŗſśŝšşșṣßťţṭŧþúùûüǔŭūũűůųụưẃẁŵẅƿýỳŷÿȳỹƴźżžẓ]$/;
+		return regex.test(name);
+	}
+};
