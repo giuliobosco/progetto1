927dcab8cfc62cb65cee5a680fdb638a50b478e5
Giulio Bosco <giuliobva@gmail.com>
2018-10-24 13:41:31 +0200

    add js emailValidation

---
+++ web/assets/js/emailValidator.js
@@ -0,0 +1,40 @@
+/**
+ * Email Validator.
+ *
+ * @author giuliobosco
+ * @version 1.0
+ */
+
+/**
+ * Email validator.
+ *
+ * @type {EmailValidator}
+ */
+let emailValidator = class EmailValidator {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param min {number} Min email length
+	 * @param max {number} Max email length.
+	 */
+	constructor(min, max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	/**
+	 * Check if the email is valid.
+	 * @param email {string} Email to validate.
+	 * @returns {boolean} Result of the validation.
+	 */
+	isValid(email) {
+
+		if ((email.trim().length > this.min && email.trim().length < this.max)) {
+			return false;
+		}
+
+		let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return email.matches(regex);
+	}
+};
