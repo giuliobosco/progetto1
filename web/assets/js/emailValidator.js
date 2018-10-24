b1f2b67d101377985df065502624356b753343ac
Giulio Bosco <giuliobva@gmail.com>
2018-10-24 13:43:30 +0200

    fix js emailValidator - regex check

--- web/assets/js/emailValidator.js
+++ web/assets/js/emailValidator.js
@@ -35,6 +35,6 @@ let emailValidator = class EmailValidator {
 		}
 
 		let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return email.matches(regex);
+		return regex.test(email);
 	}
 };
